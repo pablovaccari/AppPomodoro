9715d217af9ed54eddfce9827ef16648f17b5cda
Pablo Vaccari <pablorvaccari@gmail.com>
2022-03-01 22:24:03 -0300

    Agregar interfaz usuario e inicializar script

---
+++ main.js
@@ -0,0 +1,9 @@
+const tasks = [];
+let time = 0;
+let timer = null;
+let timerBreak = null;
+let current = null;
+
+const btnAdd = document.querySelector("#btnAdd");
+const itTask = document.querySelector("#itTask");
+const form = document.querySelector("#form");
